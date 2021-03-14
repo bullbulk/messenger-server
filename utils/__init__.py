f03b62018ea78b37714a5fb3f7da5adb499608ea
bullbulk <grur.ura@mail.ru>
2021-03-14 23:11:53 +0500

    fix

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
-from .password import encrypt_password, match_password
+from .password import encrypt_password
 from .match_params import match_required_params
 from .response import generate_response
 from .auth import UsersPool
