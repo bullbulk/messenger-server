42d91eeb0fcd56e98478d94e23dfc7a050945576
bullbulk <grur.ura@mail.ru>
2021-04-04 15:31:39 +0500

    fixes

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from app import socketio
+from app import app, socketio
 from data import db_session
 
 
 def main():
     db_session.global_init("db/messenger.db")
-    socketio.run('127.0.0.1', port=9999)
+    socketio.run(app, '127.0.0.1', port=9999)
 
 
 if __name__ == '__main__':
